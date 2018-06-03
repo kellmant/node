5f9922df2231c40f0b20865e1873ac6da9f51da6
Kellman <kellman@meghu.ca>
2018-06-03 04:06:01 -0400

    improve logging and add keystore message stream

--- msgbus/alertme.js
+++ msgbus/alertme.js
@@ -3,9 +3,9 @@ module.exports = (d,cpid) => {
 	var tstamp = Date.now() / 1000 | 0
 	const mbus = `${cpid.uid}/${tstamp} : `
 	if (typeof d === 'object') {
-		//console.dir(d, { depth: null, colors:true});
-		var newd = d.keys
-		console.dir(newd)
+		console.dir(d, { depth: null, colors:true});
+		//var newd = d.keys
+		//console.dir(newd)
 		
 	} else {
 		console.log(mbus + d);
---
+++ msgbus/stream.js
@@ -0,0 +1,12 @@
+const keySet = require('./setkey')
+
+module.exports = (d,cpid) => {
+	var tstamp = Date.now() / 1000 | 0
+	const mbus = `msg/${cpid.uid}/${tstamp}`
+	if (typeof d === 'object') {
+		let newd = JSON.stringify(d)
+		keySet(mbus, newd)	
+	} else {
+		keySet(mbus, d)	
+	}
+}
